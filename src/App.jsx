f54a410dab84a7fa75a372c5c59e953388091de6
ziani96 <81708016+ziani96@users.noreply.github.com>
2024-03-11 15:54:58 +0000

    showing individual article

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
 import { useState } from "react";
 import "./App.css";
-import { Route,Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Navigation from "./components/Navigation";
 import ArticlesList from "./components/ArticlesList";
+import ArticlePage from "./components/ArticlePage";
 
 function App() {
-  const [getArticles, setGetArticles] = useState([])
- 
+  const [getArticles, setGetArticles] = useState([]);
 
   return (
     <>
-  <h1>Scoop</h1>
-  <Navigation/>
-  <Routes>
-   <Route path="/" element={<Home/>}/>
-   <Route path="/articles" element={<ArticlesList getArticles={getArticles} setGetArticles={setGetArticles}/>}/>
-   
-
-  </Routes>
+      <h1>Scoop</h1>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/articles"
+          element={
+            <ArticlesList
+              getArticles={getArticles}
+              setGetArticles={setGetArticles}
+            />
+          }
+        />
+        <Route path="/articles/:article_id" element={<ArticlePage />} />
+      </Routes>
     </>
   );
 }
